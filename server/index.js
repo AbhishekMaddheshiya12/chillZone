8ed0532b38f3da021e783e19325947f260039183
Abhishek Maddheshiya <abhishekmaddheshiya954@gmail.com>
2025-08-23 18:12:25 +0530

    updting routes

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ cloudinary.config({
 app.use(express.json()); // middleware to parse JSON data sent in the request body
 app.use(cookieParser()); // middleware to parse cookies sent in the request header
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "chill-zone-alpha.vercel.app",
   credentials: true,
 }));
 
